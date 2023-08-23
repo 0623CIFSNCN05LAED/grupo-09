15c50d42e4e030cf1e0b92a6e09dcf217dfe6e99
Fernando-Lopez-86 <ferdinandolopez@hotmail.com>
2023-08-23 12:44:53 -0300

    creacion estructura del sitio, carpetas, rutas

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Se prendió en el puerto ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/productDetail.html"));
+});
+
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/productCart.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
