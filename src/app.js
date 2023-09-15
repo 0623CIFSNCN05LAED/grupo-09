c0decbe65be26f82757ba2fe9aa0c58cf570d7b3
NachoRSalem <nachorodriguezsalem@gmail.com>
2023-09-15 19:58:12 -0300

    archivo login

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const mainRouter = require("./routes/mainRouters");
 const app = express();
 
 
-//const PORT = 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-  //console.log(`Using port ${PORT}`);
+  console.log(`Using port ${PORT}`);
 });
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
-//app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 
 app.use(mainRouter);
