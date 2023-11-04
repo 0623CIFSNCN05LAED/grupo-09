42c9454cd66c1c29a8ea95596bdce8001319df0b
Fernando-Lopez-86 <ferdinandolopez@hotmail.com>
2023-11-04 18:59:09 -0300

    list products database

---
+++ src/database/seeders/seeders.sql
@@ -0,0 +1,49 @@
+/*DATOS PARA POBLAR LA BASE*/
+
+/*HELADERAS*/
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HEL1', 'HELADERA1', 1111, 'HELADERA1', 10, 20, 30, 40, 1, 1);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HEL2', 'HELADERA2', 2222, 'HELADERA2', 10, 20, 30, 40, 1, 1);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HEL3', 'HELADERA3', 3333, 'HELADERA3', 10, 20, 30, 40, 1, 1);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HEL4', 'HELADERA4', 4444, 'HELADERA4', 10, 20, 30, 40, 1, 1);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HEL5', 'HELADERA5', 5555, 'HELADERA5', 10, 20, 30, 40, 1, 1);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HEL6', 'HELADERA6', 6666, 'HELADERA6', 10, 20, 30, 40, 1, 1);
+
+/*LAVARROPAS*/
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LAV1', 'LAVARROPA1', 1111, 'LAVARROPA1', 10, 20, 30, 40, 1, 2);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LAV2', 'LAVARROPA2', 2222, 'LAVARROPA2', 10, 20, 30, 40, 1, 2);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LAV3', 'LAVARROPA3', 3333, 'LAVARROPA3', 10, 20, 30, 40, 1, 2);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LAV4', 'LAVARROPA4', 4444, 'LAVARROPA4', 10, 20, 30, 40, 1, 2);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LAV5', 'LAVARROPA5', 5555, 'LAVARROPA5', 10, 20, 30, 40, 1, 2);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LAV6', 'LAVARROPA6', 6666, 'LAVARROPA6', 10, 20, 30, 40, 1, 2);
+
+/*COCINAS*/
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('COC1', 'COCINA1', 1111, 'COCINA1', 10, 20, 30, 40, 1, 3);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('COC2', 'COCINA2', 2222, 'COCINA2', 10, 20, 30, 40, 1, 3);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('COC3', 'COCINA3', 3333, 'COCINA3', 10, 20, 30, 40, 1, 3);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('COC4', 'COCINA4', 4444, 'COCINA4', 10, 20, 30, 40, 1, 3);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('COC5', 'COCINA5', 5555, 'COCINA5', 10, 20, 30, 40, 1, 3);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('COC6', 'COCINA6', 6666, 'COCINA6', 10, 20, 30, 40, 1, 3);
+
+/*MICROONDAS*/
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('MIC1', 'MICROONDA1', 1111, 'MICROONDA1', 10, 20, 30, 40, 1, 4);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('MIC2', 'MICROONDA2', 2222, 'MICROONDA2', 10, 20, 30, 40, 1, 4);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('MIC3', 'MICROONDA3', 3333, 'MICROONDA3', 10, 20, 30, 40, 1, 4);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('MIC4', 'MICROONDA4', 4444, 'MICROONDA4', 10, 20, 30, 40, 1, 4);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('MIC5', 'MICROONDA5', 5555, 'MICROONDA5', 10, 20, 30, 40, 1, 4);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('MIC6', 'MICROONDA6', 6666, 'MICROONDA6', 10, 20, 30, 40, 1, 4);
+
+/*LAVAVAJILLAS*/
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LVV1', 'LAVAVAJILLA1', 1111, 'LAVAVAJILLA1', 10, 20, 30, 40, 1, 5);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LVV2', 'LAVAVAJILLA2', 2222, 'LAVAVAJILLA2', 10, 20, 30, 40, 1, 5);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LVV3', 'LAVAVAJILLA3', 3333, 'LAVAVAJILLA3', 10, 20, 30, 40, 1, 5);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LVV4', 'LAVAVAJILLA4', 4444, 'LAVAVAJILLA4', 10, 20, 30, 40, 1, 5);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LVV5', 'LAVAVAJILLA5', 5555, 'LAVAVAJILLA5', 10, 20, 30, 40, 1, 5);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('LVV6', 'LAVAVAJILLA6', 6666, 'LAVAVAJILLA6', 10, 20, 30, 40, 1, 5);
+
+/*HORNOS*/
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HOR1', 'HORNO1', 1111, 'HORNO1', 10, 20, 30, 40, 1, 6);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HOR2', 'HORNO2', 2222, 'HORNO2', 10, 20, 30, 40, 1, 6);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HOR3', 'HORNO3', 3333, 'HORNO3', 10, 20, 30, 40, 1, 6);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HOR4', 'HORNO4', 4444, 'HORNO4', 10, 20, 30, 40, 1, 6);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HOR5', 'HORNO5', 5555, 'HORNO5', 10, 20, 30, 40, 1, 6);
+INSERT INTO ecommerce.productos (sku, nombre, precio, descripcion, ancho, alto, profundidad, peso, marca_id, categoria_id) VALUES('HOR6', 'HORNO6', 6666, 'HORNO6', 10, 20, 30, 40, 1, 6);
