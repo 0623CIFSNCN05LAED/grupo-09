50b090a866e067b105f2d739a08b4021ada10805
NachoRSalem <nachorodriguezsalem@gmail.com>
2023-11-28 02:01:39 -0300

    terminadas las validaciones del register

--- src/validations/register-validation.js
+++ src/validations/register-validation.js
@@ -1,32 +1,78 @@
 const { body } = require("express-validator");
+const userServices = require("../services/userServices");
 const path = require("path");
 
 
+
 const validationsRegister = [
-    body('fullName').notEmpty().withMessage('Tienes que escribir un nombre'),
+
+    body('fullName')
+		.notEmpty()
+		.withMessage('Tienes que escribir un nombre')
+		.isLength({ min: 5 })
+        .withMessage("El nombre debe tener al menos cinco caracteres"),
+
 	body('email')
-		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
-		.isEmail().withMessage('Debes escribir un formato de correo válido'),
-	body('telefono').matches(/^\d{10}$/).withMessage('Por favor, ingresa un número de teléfono válido de 10 dígitos (solo números)'),	
-	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-	body('country').notEmpty().withMessage('Tienes que elegir un país'),
-    body('avatar').custom((value, { req }) => {
-		let file = req.file;
-		let acceptedExtensions = ['.jpg', '.png', '.gif'];
-		
-		if (!file) {
-			throw new Error('Tienes que subir una imagen');
-		} else {
-			let fileExtension = path.extname(file.originalname);
-			if (!acceptedExtensions.includes(fileExtension)) {
-				throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+		.notEmpty()
+		.withMessage("Ingresar correo eléctrónico")
+		.isEmail()
+		.withMessage("Inngresar un formato de correo electronico válido")
+		.bail()
+		.custom(async (value, { req }) => {
+			let email = req.body.email;
+			let emailToLower = email.toLowerCase();
+			let checkEmail = await userServices.getUserByEmail(emailToLower);
+			if (!checkEmail) {
+				return true;
+			} else {
+				throw new Error(`El correo electrónico ${email} ya está registrado.`);
 			}
-		}
+		}),
 
-		return true;
-	})
-] 
+	body('telefono')
+		.matches(/^\d{10}$/)
+		.withMessage('Por favor, ingresa un número de teléfono válido de 10 dígitos (solo números)'),
 
+	body('password')
+		.notEmpty()
+		.withMessage("Ingresar contraseña")
+		.bail()
+		.isLength({ min: 8 })
+		.withMessage("La contraseña debe tener al menos 8 caracteres")
+		.bail()
+		.matches(/[A-Z]/)
+		.withMessage("La contraseña debe contener al menos una letra mayúscula")
+		.bail()
+		.matches(/[a-z]/)
+		.withMessage("La contraseña debe contener al menos una letra minúscula")
+		.bail()
+		.matches(/[\d]/)
+		.withMessage("La contraseña debe contener al menos un número")
+		.bail()
+		.matches(/[!@#$%^&*(),.?":{}|<>]/)
+		.withMessage("La contraseña debe contener al menos un carácter especial"),
+
+	body('country')
+		.notEmpty()
+		.withMessage('Tienes que elegir un país'),
+
+	body("avatar").custom((value, { req }) => {
+			let file = req.file;
+			let acceptedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+	
+			if (!file) {
+				throw new Error('Seleccionar avatar');
+			} else {
+				let fileExtension = path.extname(file.originalname);
+				if (!acceptedExtensions.includes(fileExtension)) {
+					throw new Error(
+					`Las extensiones aceptadas son ${acceptedExtensions.join(", ")}`
+					);
+				}
+			}
+			return true;
+		}),
+];
 
 
 module.exports = validationsRegister;
