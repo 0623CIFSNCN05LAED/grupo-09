89b3e5b71da0a01619b59abb5e5c5e4481fc650e
Fernando-Lopez-86 <ferdinandolopez@hotmail.com>
2023-12-13 18:23:04 -0300

    modification components

--- dashboard/src/components/Peliculas.js
+++ dashboard/src/components/Peliculas.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function Peliculas(){
-    return(
-        <React.Fragment>
-            <div>PELICULAS</div>
-        </React.Fragment>
-    )
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/productos")
+        .then(response => {
+            return response.json()
+        })
+        .then(productos => {
+            setProductos(productos.data)
+        })
+    }, [])
+
+    return (
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>SKU</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Descripción</th>
+                    <th>Ancho</th>
+                    <th>Alto</th>
+                    <th>Profundidad</th>
+                    <th>Peso</th>
+                    <th>Marca</th>
+                    <th>Categoría</th>
+                </tr>
+            </thead>
+            <tbody>
+                {productos.map(producto => (
+                    <tr>
+                        <td>{producto.id}</td>
+                        <td>{producto.sku}</td>
+                        <td>{producto.nombre}</td>
+                        <td>{producto.precio}</td>
+                        <td>{producto.descripcion}</td>
+                        <td>{producto.ancho}</td>
+                        <td>{producto.alto}</td>
+                        <td>{producto.profundidad}</td>
+                        <td>{producto.peso}</td>
+                        <td>{producto.marca_id}</td>
+                        <td>{producto.categoria_id}</td>
+                    </tr>
+                ))}
+            </tbody> 
+        </table>
+    ); 
 }
+
 export default Peliculas;
+
--- dashboard/src/components/Usuarios.js
+++ dashboard/src/components/Usuarios.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function Usuarios(){
-    return(
-        <React.Fragment>
-            <div>USUARIOS</div>
-        </React.Fragment>
-    )
+    const [usuarios, setUsuarios] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/usuarios")
+        .then(response => {
+            return response.json()
+        })
+        .then(usuarios => {
+            setUsuarios(usuarios.data)
+        })
+    }, [])
+
+    return (
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>Telefono</th>
+                    <th>Pais</th>
+                </tr>
+            </thead>
+            <tbody>
+                {usuarios.map(usuario => (
+                    <tr>
+                        <td>{usuario.id}</td>
+                        <td>{usuario.fullName}</td>
+                        <td>{usuario.email}</td>
+                        <td>{usuario.telefono}</td>
+                        <td>{usuario.country}</td>
+                    </tr>
+                ))}
+            </tbody> 
+        </table>
+    ); 
 }
+
 export default Usuarios;
