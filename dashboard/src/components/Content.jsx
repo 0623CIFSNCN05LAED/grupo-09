a9f133307892d38b0976efcf392272398aa5fc75
NachoRSalem <nachorodriguezsalem@gmail.com>
2023-12-15 18:10:42 -0300

    estilos dashboard

--- dashboard/src/components/Content.jsx
+++ dashboard/src/components/Content.jsx
@@ -9,64 +9,13 @@ function Content(){
 				{/*<!-- Content Row Top -->*/}
 				<div className="container-fluid">
 					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">Dashboard Grupo 09 - Vamoooooo!</h1>
+						<h1 className="h3 mb-0 text-gray-800">Dashboard Grupo 09</h1>
 					</div>
 				
 					{/*<!-- Content Row Movies-->*/}
 					<div className="row">
 
 						{/*<!-- Total usuarios -->*/}
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-primary shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Usuarios in Data Base</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">1000</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-film fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						{/*<!-- Total productos -->*/}
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-success shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total de Productos</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">1000</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-award fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-                        {/*<!-- Categorias productos -->*/}
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-warning shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1"> Tipos de Productos</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">6</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-award fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-                        {/*<!-- Total usuarios -->*/}
 						<div className="col-md-4 mb-4">
 							<div className="card border-left-primary shadow h-100 py-2">
 								<div className="card-body">
