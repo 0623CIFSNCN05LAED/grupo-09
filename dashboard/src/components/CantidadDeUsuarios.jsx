635e5cf790a1c2664797bac9e04b91d19f5b1afc
NachoRSalem <nachorodriguezsalem@gmail.com>
2023-12-26 20:06:54 -0300

    arreglando el useEffect

--- dashboard/src/components/CantidadDeUsuarios.jsx
+++ dashboard/src/components/CantidadDeUsuarios.jsx
@@ -3,28 +3,30 @@ import { useState, useEffect } from 'react';
 
 
 const CantidadDeUsuariuos = () => {
-	let cantidadUsuarios = [ ];
+    const [cantidadUsuarios, setCantidadUsuarios] = useState(0);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/usuarios')
-      .then(response => response.json())
-      .then(data => {
-        cantidadUsuarios = data.cantidadUsuarios;
-      });
-  }, []);
+    useEffect(() => {
+      const obtenerCantidadUsuarios = async () => {
+        const response = await fetch('http://localhost:3000/api/usuarios');
+        const { cantidadUsuarios } = await response.json();
+        setCantidadUsuarios(cantidadUsuarios);
+      };
+  
+      obtenerCantidadUsuarios();
+    }, []);
 
+    console.log(" cantidadUsuarios ", cantidadUsuarios);
 
   return(
 	<React.Fragment>
-
-				{/*<!-- Total usuarios -->*/}
+					{/*<!-- Total usuarios -->*/}
 					<div className="col-md-4 mb-4">
 						<div className="card border-left-primary shadow h-100 py-2">
 							<div className="card-body">
 								<div className="row no-gutters align-items-center">
 									<div className="col mr-2">
 										<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Usuarios</div>
-										<div className="h5 mb-0 font-weight-bold text-gray-800">{cantidadUsuarios}</div>
+										<div className="">{cantidadUsuarios}</div>
 									</div>
 									<div className="col-auto">
 										<i className="fas fa-film fa-2x text-gray-300"></i>
