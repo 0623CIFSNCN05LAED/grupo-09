77eba7696ba7abb077ca3b3428327c3d16b91b9f
NachoRSalem <nachorodriguezsalem@gmail.com>
2023-12-26 19:46:05 -0300

    cantidad de productos y usuarios dasboard

--- dashboard/src/components/Content.jsx
+++ dashboard/src/components/Content.jsx
@@ -1,75 +1,66 @@
 import React from 'react';
 import Productos from './Productos'
 import Usuarios from './Usuarios'
+import CantidadDeUsuariuos from "./CantidadDeUsuarios"
+import CantidadDeProductos from './CantidadDeProductos';
+import { useState, useEffect } from 'react';
 
-function Content(){
+const Contetnt = () => {
+	let cantidadUsuarios = [ ];
 
-    return(
-        <React.Fragment>
-				{/*<!-- Content Row Top -->*/}
-				<div className="container-fluid">
-                    
-					{/*<!-- Content Row Movies-->*/}
-					<div className="row">
+  useEffect(() => {
+    fetch('http://localhost:3000/api/usuarios')
+      .then(response => response.json())
+      .then(data => {
+        cantidadUsuarios = data.cantidadUsuarios;
+      });
+  }, []);
 
-						{/*<!-- Total usuarios -->*/}
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-primary shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Usuarios</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">1000</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-film fa-2x text-gray-300"></i>
-										</div>
+
+  return(
+	<React.Fragment>
+			{/*<!-- Content Row Top -->*/}
+			<div className="container-fluid">
+			<div className="row">
+
+				{/*<!-- Cantidad Usuarios -->*/}
+				
+
+				     <CantidadDeUsuariuos/>
+
+
+					{/*<!-- Total productos -->*/}
+				
+                    <CantidadDeProductos/>
+
+					{/*<!-- Categorias productos -->*/}
+					<div className="col-md-4 mb-4">
+						<div className="card border-left-warning shadow h-100 py-2">
+							<div className="card-body">
+								<div className="row no-gutters align-items-center">
+									<div className="col mr-2">
+										<div className="text-xs font-weight-bold text-warning text-uppercase mb-1"> Tipos de Productos</div>
+										<div className="h5 mb-0 font-weight-bold text-gray-800">6</div>
+									</div>
+									<div className="col-auto">
+										<i className="fas fa-award fa-2x text-gray-300"></i>
 									</div>
 								</div>
 							</div>
 						</div>
-
-						{/*<!-- Total productos -->*/}
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-success shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total de Productos</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">1000</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-award fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-                        {/*<!-- Categorias productos -->*/}
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-warning shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1"> Tipos de Productos</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">6</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-award fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-                        <Productos/>
-                        <Usuarios/>
-                        
 					</div>
+
+					<Productos/>
+					<Usuarios/>
+					
 				</div>
-        </React.Fragment>
-    )
+			</div>
+	</React.Fragment>
+)
 }
 
-export default Content;
+
+export default Contetnt;
+
+
+
